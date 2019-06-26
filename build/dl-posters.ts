7b2e0177cebc031f71334f51b59fdb0c04a4723f
Coteh <james_c_34@live.com>
2019-06-26 18:58:30 -0400

    Add dl-poster retry files

--- build/dl-posters.ts
+++ build/dl-posters.ts
@@ -28,32 +28,52 @@ let downloadMapStr: Buffer = fs.readFileSync(path.resolve(BASE_BUILD_PATH, "movi
 let downloadMap: string = JSON.parse(downloadMapStr.toString());
 
 for (let i = 1; i <= NUMBER_OF_MCU_MOVIES; i++) {
-    // does the file exist in the images folder?
     let filename: string = i.toString() + ".jpg";
-    if (fileMap[filename]) {
+    let retry: boolean = fs.existsSync(getRetryPath(path.resolve(BASE_DOWNLOAD_PATH, filename)));
+
+    // does the file exist in the images folder? and is there no retry file present?
+    if (fileMap[filename] && !retry) {
         console.log(i + " exists, skipping...");
     } else {
         // if not then if building for prod dl from imdb and put into images folder
         // if on test then copy test image into images folder instead
         let destinationPath: string = path.resolve(BASE_DOWNLOAD_PATH, filename);
+
         if (process.env.NODE_ENV === "test") {
             console.log("Copying test poster to " + destinationPath);
             copyPlaceholderPoster(path.resolve(BASE_TEST_IMG_PATH, "test.jpg"), destinationPath);
+            writeRetryFile(filename, getRetryPath(destinationPath));
         } else {
             let dlFilename: string = downloadMap[i];
+
             console.log("Downloading poster " + dlFilename + " from IMDB to " + destinationPath + "...");
             request(dlFilename).on("error", (err) => {
                 console.error("Error downloading poster. '" + err + "' - Using fallback image.");
                 copyPlaceholderPoster(path.resolve(BASE_TEST_IMG_PATH, "test.jpg"), destinationPath);
+                writeRetryFile(filename, getRetryPath(destinationPath));
             }).pipe(fs.createWriteStream(destinationPath)).on("close", () => {
                 console.log("Downloaded poster to " + destinationPath);
+                if (retry) {
+                    console.log("Deleting retry file");
+                    fs.unlinkSync(getRetryPath(destinationPath));
+                }
             });
         }
     }
+}
+
+function getRetryPath(path: string) {
+    return path + ".retry";
 }
 
 function copyPlaceholderPoster(sourcePath: string, destPath: string) {
     fs.promises.copyFile(sourcePath, destPath)
         .then(() => console.log("Placeholder poster copied to " + destPath))
         .catch(() => console.error("Error copying placeholder poster to " + destPath));
+}
+
+function writeRetryFile(filename: string, destinationPath: string) {
+    console.log("Adding retry file for poster " + filename);
+    fs.writeFileSync(destinationPath, "");
+    console.log("Retry file added for " + filename);
 }
